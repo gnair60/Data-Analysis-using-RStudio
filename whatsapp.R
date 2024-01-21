d70b87e1178c9ef246af4e769752e29c0f316125
gnair60 <gautamnair60@gmail.com>
2024-01-21 01:30:37 -0700

    Update whatsapp.R

--- whatsapp.R
+++ whatsapp.R
@@ -17,8 +17,9 @@ text=texts;
 docs <- Corpus(VectorSource(text))
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
 docs <- tm_map(docs, toSpace, "\\|")
+docs <- tm_map(docs, toSpace, "@")
+
 docs <- tm_map(docs, content_transformer(tolower))
 docs <- tm_map(docs, removeNumbers)
 docs <- tm_map(docs, removeWords, stopwords("english"))
